fd346971ae4d34ed10a5c9a1b73f5b10e531de9f
Fenando Denis <jmoran5@ucol.mx>
2023-05-08 21:40:13 -0600

    first commit

---
+++ Club.rb
@@ -0,0 +1,18 @@
+class Human
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def sing
+        puts "♫ ♫ Baby don't hurt me, don't hurt me, no more ♫ ♫"
+    end
+
+    def introduce
+        puts "Hi, my name is #{@name} and I'm #{@age} years old."
+    end
+end
+
+Pedro = Human.new('Pedro', 15)
+Pedro.sing
+Pedro.introduce
